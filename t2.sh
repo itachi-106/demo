cfd9cd11d87786a6e8166bbfd4cd756936c904c9
itachi-106 <mumairh06@gmail.com>
2022-08-02 17:53:24 +0500

    added count narration with each increment

--- t2.sh
+++ t2.sh
@@ -3,6 +3,11 @@ a=1
 while [ $a -le 10 ] 
 do
 	echo " $a "
-	echo "incremented ohyh"
+	if [ $a -eq 10 ]
+	then
+		echo "max count reached"
+	else
+		echo " $a count reached"
+	fi
 	a=$(( $a + 1))
 done
